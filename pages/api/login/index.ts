2b7504d7aa6b01c00495e9d00b48671106b6519b
brandonhsz <brandonhszzz@gmail.com>
2022-05-24 22:53:37 -0500

    fixed next/server error

--- pages/api/login/index.ts
+++ pages/api/login/index.ts
@@ -1,9 +1,10 @@
-import { NextRequest, NextResponse } from "next/server"
 import { Mongo } from "../../../src/utils/Mongo.config"
 
 const db = Mongo.getInstance()
 db.connect()
 
 export default async function getMethod(req, res) {
-
+  res.json({
+    messagge: "building"
+  })
 }
