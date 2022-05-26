dccb9eb2968f01aaf0ad688aae69490c4efdf3a9
brandonhsz <brandonhszzz@gmail.com>
2022-05-25 22:17:12 -0500

    JWT ADDED

--- src/components/qualifications/TableContent/TableContent.tsx
+++ src/components/qualifications/TableContent/TableContent.tsx
@@ -9,6 +9,7 @@ const TableContent = ({ img, alt }: IProps) => {
   return (
     <div className='flex flex-col justify-end'>
       <img className={`${styles.center}`} height="180px" width="120px" src={img} alt={alt} />
+
       <p className={`${styles['table-text']}`}>{alt}</p>
     </div>
   )
