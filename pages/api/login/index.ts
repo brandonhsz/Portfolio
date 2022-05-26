2aca8d7a1f200b09a6870e83a3757504f8b9d13a
brandonhsz <brandonhszzz@gmail.com>
2022-05-25 23:08:48 -0500

    nextjs cors added

--- pages/api/login/index.ts
+++ pages/api/login/index.ts
@@ -1,13 +1,20 @@
 import { Mongo } from "../../../src/utils/Mongo.config"
 import Admin from "../../../src/Models/Admins.schema"
 import jwt from "jsonwebtoken"
-
+import NextCors from 'nextjs-cors';
 
 
 const db = Mongo.getInstance()
 db.connect()
 
 export default async function getMethod(req, res) {
+
+  await NextCors(req, res, {
+    // Options
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+    origin: '*',
+    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+  });
 
   const { name, password } = JSON.parse(req.body)
 
