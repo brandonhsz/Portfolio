0cc2f4e0d8a72c86573652b8407df99fe17e1991
brandonhsz <brandonhszzz@gmail.com>
2022-05-25 22:30:02 -0500

    cors added

--- pages/api/login/index.ts
+++ pages/api/login/index.ts
@@ -1,11 +1,24 @@
+import Cors from "cors"
+import initMiddleware from "libs/init.middleware"
 import { Mongo } from "../../../src/utils/Mongo.config"
 import Admin from "../../../src/Models/Admins.schema"
 import jwt from "jsonwebtoken"
+
+const cors = initMiddleware(
+  // You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
+  Cors({
+    // Only allow requests with GET, POST and OPTIONS
+    methods: ['GET', 'POST', 'OPTIONS'],
+  })
+)
 
 const db = Mongo.getInstance()
 db.connect()
 
 export default async function getMethod(req, res) {
+
+  await cors(req, res)
+
   const { name, password } = JSON.parse(req.body)
 
   const isAdmin = await Admin.findOne({ name: name })
